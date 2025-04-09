2f74f277c0cc3d539b0939244bc00d9235b3489b
AyonDevNet <rmcoder7@gmail.com>
2025-04-09 15:10:36 +0600

    add

---
+++ calculator.sh
@@ -0,0 +1,51 @@
+echo "Welcome to Bash Calculator!"
+echo
+echo "Available operations: add, subtract, multiply, divide"
+echo
+read -p "Enter operation: " operation  #when it will ask you about what wanna do, like , add, substract etc.
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+
+
+#main function area
+add() {
+
+       echo "Result" $(($1+$2))
+
+}
+subtreaction() {
+
+       echo "Result" $(($1-$2))
+
+}
+multi() {
+
+       echo "Result" $(($1*$2))
+
+}
+divided() {
+       
+       if [ $2 -eq 0 ]; then
+         echo "this is invalid"
+       else
+            echo "Result " $($1/$2)
+   
+}
+
+ if [ $operation = add]; then
+     add $num1 $num1
+
+ elif [ $operation = subtreaction]; then
+     subtreaction $num1 $num2
+ 
+ elif [ $operation = multi]; then
+     multi $num1 $num2
+ elif [ $operation = divided]; then
+     divided $num1 $num2
+ else 
+     echo "Invalid operation"
+  fi
+
+
+
+
